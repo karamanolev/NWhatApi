561babcf814ae94697d889ead9bdc23503f5e62f
Ivailo Karamanolev <ivailo@karamanolev.com>
2012-10-02 09:53:33 +0300

    Add GetGroupTorrentInfoById method.

--- NWhatApi/Model/TorrentGroupResponse.cs
+++ NWhatApi/Model/TorrentGroupResponse.cs
@@ -12,6 +12,11 @@ namespace NWhatApi.Model
         [JsonProperty("torrents")]
         public GroupTorrentInfo[] Torrents { get; set; }
 
+        public GroupTorrentInfo GetGroupTorrentInfoById(long torrentId)
+        {
+            return this.Torrents.Where(t => t.Id == torrentId).FirstOrDefault();
+        }
+
         public string GetTorrentFileName(GroupTorrentInfo torrentInfo)
         {
             return this.Group.MusicInfo.JoinedArtists + " - " + this.Group.Name + " - " + this.Group.Year + " (" + torrentInfo.Media + " - " + torrentInfo.Format + " - " + torrentInfo.Encoding + ").torrent";
